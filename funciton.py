ab68a220e80ee0e00bfa0bd6be63c1157970f83e
yasemindev211 <yaseminbulat16@gmail.com>
2026-03-20 00:41:34 +0300

    Delete funciton.py

--- funciton.py
+++
@@ -1,9 +0,0 @@
-"""
-1. Gönderilen bir kelimeti belirtilen kez ekrana fonk. yaz
-2. Kendine gönderilen sınırsız sayıdak parametreyi bir listeye çeviren fonk.yaz
-3. Gönderilen 2 sayı arasındaki tüm asal sayıları bulun.
-4. Knedisine gönderilen bir sayının tam bölnelerini bir liste şeklinde döndür."""
-
-def yazdir(kelime, adet):
-    print(kelime * adet)
-    yazdir('Merhaba\n', 10)    
